9c4ef5236613918460cc14f7bb69be868739c573
Wojciech Nowaczyk <wojciechnowaczyk@MacBook-Pro-Wojciech.local>
2019-11-13 12:32:27 +0100

    Header created

--- kancelariaKopania/src/pages/services.js
+++ kancelariaKopania/src/pages/services.js
@@ -1,16 +1,48 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import books from "../images/books.jpg"
+
 const Services = () => (
-  <Layout>
+  <>
     <SEO title="Services" />
-    <h1>Services</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Header>
+      <HeaderOpacity>
+        <HeaderTitle>
+          Usługi
+        </HeaderTitle>
+      </HeaderOpacity>
+    </Header>
+  </>
 )
+
+const Header = styled.div`
+  width: 100%;
+  height: 75vh;
+  background-image: url(${books});
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const HeaderOpacity = styled.div`
+  width: 70%;
+  height: 50%;
+  background: rgba(134, 89, 45, 0.9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const HeaderTitle = styled.h1`
+  color: white;
+  font-size: 40px;
+`
 
 export default Services

854a699d724d1c5bc950cd4b189cf0649f337126
Wojciech Nowaczyk <wojciechnowaczyk@MacBook-Pro-Wojciech.local>
2019-11-13 17:22:47 +0100

    Services page

---
+++ kancelariaKopania/src/components/menu.js
@@ -0,0 +1,49 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const Menu = () => {
+  return(
+  <Box>
+    <MenuList>
+      <Line>
+        <Link to="/" style={LinkStyles}> O kancelarii </Link>
+      </Line>
+      <Line>
+        <Link to="/services/" style={LinkStyles}> Usługi </Link>
+      </Line>
+      <Line>
+        <Link to="/costs/" style={LinkStyles}> Wynagrodzenie </Link>
+      </Line>
+      <Line>
+        <Link to="/contact/" style={LinkStyles}> Kontakt </Link>
+      </Line>
+    </MenuList>
+  </Box>
+)}
+
+const Box = styled.div`
+  width: 14%;
+  border: 2px solid rgb(134, 89, 45);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  margin-left: 1%;
+  position: absolute;
+`
+const MenuList = styled.ul`
+  list-style-type: none;
+  margin: 0px;
+`
+const Line = styled.li`
+  color: rgb(134, 89, 45);
+  margin-top: calc(1.45rem / 2);
+`
+
+const LinkStyles = {
+  color: 'black',
+  textDecoration: 'none',
+}
+
+export default Menu
--- kancelariaKopania/src/pages/services.js
+++ kancelariaKopania/src/pages/services.js
@@ -1,25 +1,68 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PropTypes from "prop-types"
+
+import Menu from "../components/menu"
 
 import books from "../images/books.jpg"
 
-const Services = () => (
-  <>
-    <SEO title="Services" />
-    <Header>
-      <HeaderOpacity>
-        <HeaderTitle>
-          Usługi
-        </HeaderTitle>
-      </HeaderOpacity>
-    </Header>
-  </>
-)
+class Services extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showService: false };
+    this.toggleBox = this.toggleBox.bind(this);
+  }
+  toggleBox() {
+  const { showService } = this.state;
+  this.setState({
+    showService: !showService,
+  });
+}
+  render(){
+    return(
+      <>
+        <SEO title="Services" />
+        <Header>
+          <HeaderOpacity>
+            <HeaderTitle>
+              Usługi
+            </HeaderTitle>
+          </HeaderOpacity>
+        </Header>
+        <Menu />
+        <MainBody>
+          <ServiceBox onClick={this.state.toggleBox}>
+            <ServiceTitle>
+              Zakładanie i obsługa prawna spółek, KRS
+            </ServiceTitle>
+            {this.state.showService === true && <ServiceBody>
+            Prowadzenie każdej firmy wymaga rzetelnej, profesjonalnej i skutecznej obsługi prawnej. Kompleksowo dbamy o kwestie prawne Państwa firmy, dlatego:
+            <ul>
+              <li>Doradzamy przy założeniu spółki</li>
+              <li>Kompletujemy wymagane dokumenty </li>
+              <li>Przeprowadzamy rejestrację spółek oraz dokonujemy późniejszych zmian w KRS</li>
+              <li>Przeprowadzamy audyt prawny (badanie spółek)</li>
+              <li>Reprezentujemy w postępowaniach w sprawie nieważności uchwał</li>
+              <li>Prowadzimy sprawy korporacyjne – w tym przygotowywanie dokumentacji na Zgromadzenia Wspólników oraz posiedzenia Zarządu</li>
+              <li>Opracowujemy projekty uchwał</li>
+              <li>Świadczymy kompleksową pomoc w łączeniu bądź podziale spółek</li>
+              <li>Sporządzamy umowy handlowe i pracownicze</li>
+              <li>Reprezentujemy spółki przed sądami</li>
+              <li>Wszczynamy oraz monitorujemy przebieg postępowania egzekucyjnego i zabezpieczającego</li>
+              <li>Prowadzimy windykację należności</li>
+              <li>Podejmujemy działania zmierzające do ochrony przed nieuczciwą konkurencją</li>
+            </ul>
+            </ServiceBody>}
+          </ServiceBox>
+        </MainBody>
+      </>
+  )}
+  }
 
 const Header = styled.div`
   width: 100%;
@@ -34,7 +77,7 @@ const Header = styled.div`
 const HeaderOpacity = styled.div`
   width: 70%;
   height: 50%;
-  background: rgba(134, 89, 45, 0.9);
+  background: rgba(134, 89, 45, 0.7);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -43,6 +86,26 @@ const HeaderOpacity = styled.div`
 const HeaderTitle = styled.h1`
   color: white;
   font-size: 40px;
+`
+
+const MainBody = styled.div`
+  width: 70%;
+  margin-left: 15%;
+  margin-top: 20px;
+  padding: 20px;
+`
+
+const ServiceBox = styled.div`
+  width: 500px;
+  border: 1px solid black;
+`
+
+const ServiceTitle = styled.p`
+  border: 1px solid green;
+`
+
+const ServiceBody = styled.div`
+  border: 1px solid red;
 `
 
 export default Services
